a3d678c288070d8769c286fdbb12d27d43472267
Maya Zini <mayazini05@gmail.com>
2023-03-21 23:26:21 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+import Home from './pages/home';
+import Login from './pages/login';
+import About from './pages/about';
+import Footer from './components/footer';
+import './style.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+        <div className="app-wrapper">
+    <Navbar />
+    <Routes>
+        <Route path='/home' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/about' element={<About/>} />
+    </Routes>
     </div>
-  );
+    <Footer/>
+    </Router>
+
+);
 }
 
 export default App;
